dbb2d9cbaa059c25e90565d5e32bbb8c5f14d2a5
abel-solo <abel.solo2011@yahoo.com>
2022-07-12 15:25:06 -0700

    adding a variadic function

--- _printf.c
+++ _printf.c
@@ -2,6 +2,41 @@
 #include <stdlib.h>
 
 /**
+  * check_for_specifiers - checks if there is a valid specifier
+  * @format: possible format specifier
+  * Return: pointer to valid function
+  */
+static int (*check_for_specifiers(const char *format))(va_list)
+{
+	unsigned int i;
+	print_t p[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{"i", print_i},
+		{"d", print_d},
+		{"u", print_u},
+		{"b", print_b},
+		{"o", print_o},
+		{"x", print_x},
+		{"X", print_X},
+		{"p", print_p},
+		{"S", print_S},
+		{"r", print_r},
+		{"R", print_R},
+		{NULL, NULL}
+		};
+
+		for (i = 0; p[i].t != NULL; i++)
+		{
+			if (*(p[i].t) == *format)
+			{
+				break;
+			}
+		}
+		return (p[i].f);
+}
+
+/**
   * _printf - it prints anything
   * @format: possible format specifier
   * Return: the characters it printed
@@ -24,7 +59,7 @@ int _printf(const char *format, ...)
 		}
 		if (!format[i])
 			return (count);
-		
+		f = check_for_specifiers(&format[i + 1]);
 		if (f != NULL)
 		{
 			count += f(valist);
